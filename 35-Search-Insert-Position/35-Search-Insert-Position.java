d9f586a035bdbce8324e41e44b1a01022e1c603d
farazxsiddiqui <ahmedfarazsiddiqui1@gmail.com>
2022-04-16 02:34:52 +0530

    35-Search-Insert-Position

---
+++ 35-Search-Insert-Position/35-Search-Insert-Position.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==target)
+            {
+                return i;
+            }
+        }
+    
+        for(int i=0;i<nums.length-1;i++)
+            {
+             if(nums[i]<target&&nums[i+1]>target)
+            {
+                return i+1;
+            }
+            
+        }
+        
+        if(nums[nums.length-1]<target)
+            {
+                return nums.length;
+            }
+        return 0;
+        
+    }
+}
