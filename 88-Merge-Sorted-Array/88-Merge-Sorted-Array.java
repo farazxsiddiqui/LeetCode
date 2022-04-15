78b8cb395cb159bdcb2c193e970f071a97ae1d74
farazxsiddiqui <ahmedfarazsiddiqui1@gmail.com>
2022-04-16 02:10:56 +0530

    88-Merge-Sorted-Array

---
+++ 88-Merge-Sorted-Array/88-Merge-Sorted-Array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for(int i =m;i<m+n;i++)
+        {
+            if(nums1[i]==0)
+            {
+                nums1[i]=nums2[i-m];
+            }
+        }
+        Arrays.sort(nums1); 
+    }
+}
